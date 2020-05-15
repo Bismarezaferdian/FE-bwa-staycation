bef2c3a879a2cc8c7b73f79e3e8babdae934ea17
bismarezaferdian <bismarezaferdian95@gmail.com>
2020-05-15 13:51:16 +0700

    Fix LandingPage

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,18 +1,17 @@
 import React from "react";
-
 import Button from "elements/Button";
-import BrandIcon from "parts/IconText";
+import Icon from "parts/IconText";
 
 export default function Header(props) {
   const getNavLinkClass = (path) => {
-    return props.location.pathname === path ? "active" : "";
+    return props.location.pathname === path ? " active" : "";
   };
 
   return (
     <header className="spacing-sm">
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light">
-          <BrandIcon />
+          <Icon />
 
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav ml-auto">
@@ -23,7 +22,7 @@ export default function Header(props) {
               </li>
               <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
                 <Button className="nav-link" type="link" href="/browse-by">
-                  Browse-by
+                  Browse By
                 </Button>
               </li>
               <li className={`nav-item${getNavLinkClass("/stories")}`}>
